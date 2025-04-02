204d3beb5ec980936c633d6a756e7f780d4e5244
JuliaAraujoOliveira <julia.oliveira@sptech.school>
2025-04-02 20:01:39 -0300

    Preparando ambiente para o CRUD

--- Back_End/preciososLacos/src/main/kotlin/com/lacos_preciosos/preciososLacos/dto/UsuarioDTO.kt
+++ Back_End/preciososLacos/src/main/kotlin/com/lacos_preciosos/preciososLacos/dto/UsuarioDTO.kt
@@ -1,12 +1,18 @@
 package com.lacos_preciosos.preciososLacos.dto
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import java.time.LocalDate
 
 
 @JsonIgnoreProperties
 data class UsuarioDTO(
+
     var nome: String,
     var email: String,
-    var cep: String,
-    var numeroCasa: Int
+    var senha: String,
+    var cpf: String,
+    var telefone: String,
+    @JsonIgnore var data_cadastro: LocalDate
+
 )
--- Back_End/preciososLacos/src/main/kotlin/com/lacos_preciosos/preciososLacos/model/Usuario.kt
+++ Back_End/preciososLacos/src/main/kotlin/com/lacos_preciosos/preciososLacos/model/Usuario.kt
@@ -1,23 +1,29 @@
 package com.lacos_preciosos.preciososLacos.model
 
 import com.lacos_preciosos.preciososLacos.dto.UsuarioDTO
+import java.time.LocalDate
 
 class Usuario {
-
+    var id: Int = 0;
     var nome: String = "";
     var email: String = "";
-    var cep: String = "";
-    var numeroCasa: Int = 1;
+    var senha: String = "";
+    var telefone: String = "";
+    var data_cadastro: LocalDate;
+    var cpf: String = "";
 
     constructor(usuarioDTO: UsuarioDTO) {
         this.nome = usuarioDTO.nome;
+        this.senha = usuarioDTO.senha;
+        this.telefone = usuarioDTO.telefone;
+        this.data_cadastro = LocalDate.now();
         this.email = usuarioDTO.email;
-        this.cep = usuarioDTO.cep;
-        this.numeroCasa = usuarioDTO.numeroCasa;
+        this.cpf = usuarioDTO.cpf;
+
     }
 
     override fun toString(): String {
-        return "Usuario(nome='$nome', email='$email', cep='$cep', numeroCasa=$numeroCasa)"
+        return "Usuario(nome='$nome', email='$email', cpf='$cpf',senha = '$senha', telefone = '$telefone', data_cadastro = $data_cadastro)"
     }
 
 }
