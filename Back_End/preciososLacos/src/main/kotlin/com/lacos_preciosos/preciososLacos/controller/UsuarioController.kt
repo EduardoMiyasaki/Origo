5ec8daa7895dd10008debed603f09711ffb1e7da
JuliaAraujoOliveira <julia.oliveira@sptech.school>
2025-04-06 21:13:11 -0300

    adicionando a inserção de usuario no banco de dados

--- Back_End/preciososLacos/src/main/kotlin/com/lacos_preciosos/preciososLacos/controller/UsuarioController.kt
+++ Back_End/preciososLacos/src/main/kotlin/com/lacos_preciosos/preciososLacos/controller/UsuarioController.kt
@@ -2,24 +2,32 @@ package com.lacos_preciosos.preciososLacos.controller
 
 import com.lacos_preciosos.preciososLacos.dto.UsuarioDTO
 import com.lacos_preciosos.preciososLacos.model.Usuario
+import com.lacos_preciosos.preciososLacos.repository.UsuarioRepository
 import com.lacos_preciosos.preciososLacos.service.UsuarioService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/usuarios")
-class UsuarioController(private val usuarioService: UsuarioService) {
+class UsuarioController(private val repositorio: UsuarioRepository) {
 
-    val usuariosList = mutableListOf<Usuario>();
+    // val usuariosList = mutableListOf<Usuario>();
 
-    @PostMapping
+  /*  @PostMapping
     fun criarUsuario(@RequestBody usuarioDTO: UsuarioDTO): ResponseEntity<Usuario> {
         val usuario = usuarioService.criarUsuario(usuarioDTO);
         usuariosList.add(usuario);
         return ResponseEntity.status(201).body(usuario)
+    }*/
+
+    //Inserindo o usuario no banco de Dados
+    @PostMapping
+    fun cadastrarUsuario(@RequestBody novoUsuario: Usuario): ResponseEntity<Usuario> {
+        val usuario = repositorio.save(novoUsuario)
+        return ResponseEntity.status(201).body(usuario)
     }
 
-    @GetMapping
+  /*  @GetMapping
     fun listarUsuario(): ResponseEntity<List<Usuario>> {
         if (usuariosList.isEmpty()) {
             return ResponseEntity.status(204).build()
@@ -70,5 +78,5 @@ class UsuarioController(private val usuarioService: UsuarioService) {
         }
         return ResponseEntity.status(404).build();
     }
-
+*/
 }
