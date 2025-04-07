caaa9cc43e7876e7ddd4927a6dffdd541142def8
JuliaAraujoOliveira <julia.oliveira@sptech.school>
2025-04-06 21:41:32 -0300

    realizando alterações no codigo para evitar conflitos

--- Back_End/preciososLacos/src/main/kotlin/com/lacos_preciosos/preciososLacos/model/Usuario.kt
+++ Back_End/preciososLacos/src/main/kotlin/com/lacos_preciosos/preciososLacos/model/Usuario.kt
@@ -1,29 +1,37 @@
 package com.lacos_preciosos.preciososLacos.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.lacos_preciosos.preciososLacos.dto.UsuarioDTO
+import jakarta.persistence.*
 import java.time.LocalDate
+ @Entity
+data class Usuario(
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Id // do pacote jakarta.persistence
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-class Usuario {
-    var id: Int = 0;
-    var nome: String = "";
-    var email: String = "";
-    var senha: String = "";
-    var telefone: String = "";
-    var data_cadastro: LocalDate;
-    var cpf: String = "";
+    @Column(name= "id_usuario")
+    var idUsuario: Int? = null,
 
-    constructor(usuarioDTO: UsuarioDTO) {
-        this.nome = usuarioDTO.nome;
-        this.senha = usuarioDTO.senha;
-        this.telefone = usuarioDTO.telefone;
-        this.data_cadastro = LocalDate.now();
-        this.email = usuarioDTO.email;
-        this.cpf = usuarioDTO.cpf;
+    @Column(name = "nome_completo")
+    var nomeCompleto: String,
 
-    }
-
+    var email: String,
+    var senha: String,
+    var cpf: String,
+    var telefone: String,
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    var data_cadastro: LocalDate = LocalDate.now()
+) {
+    constructor(usuarioDTO: UsuarioDTO): this(
+        nomeCompleto = usuarioDTO.nomeCompleto,
+        senha = usuarioDTO.senha,
+        telefone = usuarioDTO.telefone,
+        email = usuarioDTO.email,
+        cpf = usuarioDTO.cpf
+    )
     override fun toString(): String {
-        return "Usuario(nome='$nome', email='$email', cpf='$cpf',senha = '$senha', telefone = '$telefone', data_cadastro = $data_cadastro)"
+        return "Usuario(nome='$nomeCompleto', email='$email', cpf='$cpf',senha = '$senha', telefone = '$telefone', data_cadastro = $data_cadastro)"
     }
 
 }
